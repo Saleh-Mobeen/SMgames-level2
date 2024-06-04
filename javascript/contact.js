d1dc22efce4c065de9d6a028ae4c4b56e2ed44a8
Saleh-Mobeen <165857897+Saleh-Mobeen@users.noreply.github.com>
2024-06-04 11:54:33 +0500

    Add files via upload

--- javascript/contact.js
+++ javascript/contact.js
@@ -13,36 +13,36 @@ const submit = document.getElementById('submit')
 
 
 function sendEmail() {
-    emailjs.send("service_irnsb7c", "template_f1ytmzs", { name: formValues.name , email: formValues.email , subject: formValues.subject , message: formValues.message }).then(function (response) {
+    emailjs.send("service_irnsb7c", "template_f1ytmzs", { name: formValues.name, email: formValues.email, subject: formValues.subject, message: formValues.message }).then(function (response) {
+
         submit.value = "sent ✔"
         submit.style.pointerEvents = 'none'
+
         console.log(response);
-    }, function (error) {        
+    }, function (error) {
+
         submit.value = "message can't be sent"
         submit.style.pointerEvents = 'none'
 
         setTimeout(() => {
             submit.value = "send"
             submit.style.pointerEvents = 'all'
-        },3000)
+        }, 3000)
+
         console.log(error);
     });
 }
 
 
-
-
-
-
 const form = document.getElementById('form')
-let formValues = {}; 
+let formValues = {};
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    submit.value = "sending..."
+    submit.style.pointerEvents = "none"
+    const inputs = form.querySelectorAll('input, textarea');
 
-    const inputs = form.querySelectorAll('input, textarea'); // Select both inputs and textarea
-
-    console.log(inputs);
 
     for (var i = 0; i < inputs.length; i++) {
         formValues[inputs[i].name] = inputs[i].value;
